eb26d55e5ee5954bc8b5bb31ffc1922d1de2a37e
admin <vsevolod.s.bondar@gmail.com>
2026-03-18 13:43:06 +0200

    Just comments.

--- player/stoppable_streamer.go
+++ player/stoppable_streamer.go
@@ -1,21 +1,20 @@
 package player
 
 import (
-	"fmt"
 	"sync/atomic"
 
 	"github.com/gopxl/beep/v2"
 )
 
+// wrapper around beep.Streamer to allow stopping the stream from another goroutine
 type stoppableStreamer struct {
 	inner   beep.Streamer
-	stopped *atomic.Bool
+	stopped *atomic.Bool // indicates whether the stream has been stopped already assuming that the stream can be stopped from another goroutine
 	err     error
 }
 
-func (s *stoppableStreamer) Stream(samples [][2]float64) (n int, ok bool) {
+func (s *stoppableStreamer) Stream(samples [][2]float64) (n int, ok bool) { //pointed to specific stoppableStreamer to change it's state
 	if s.stopped.Load() {
-		s.err = fmt.Errorf("stream stopped")
 		return 0, false
 	}
 	n, ok = s.inner.Stream(samples)
